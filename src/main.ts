02540f8b363bb6871e829d42473da3b4993b8b2c
Khousheish <khousheish@live.com>
2023-10-25 14:23:21 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+import { REDUCER_PROVIDER, getInitialState, reducerToken } from '@AppStore';
+import { environment } from '@Environment';
+
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { MultiTranslateLoader } from './app/shared/loaders/multi-translate.loader';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: MultiTranslateLoader,
+          deps: [HttpClient],
+        },
+      }),
+    ),
+    provideEffects([]),
+    provideStore(reducerToken, { initialState: getInitialState }),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    REDUCER_PROVIDER,
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err: unknown) => console.error(err));
