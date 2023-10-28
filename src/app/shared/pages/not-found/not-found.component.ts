389c1f3bee41d7df16e44c73eaef475f2d79eed3
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-10-28 11:56:36 +0200

    Change App Name And Selector Prefix

--- src/app/shared/pages/not-found/not-found.component.ts
+++ src/app/shared/pages/not-found/not-found.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'ngt-not-found',
+  selector: 'lu-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
   standalone: true,
