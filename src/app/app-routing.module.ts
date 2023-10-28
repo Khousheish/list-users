0c1c5b8ae4681d0c0bbc51b0b79d834a449c169d
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-10-28 12:00:57 +0200

    Configure Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutes } from '@Enums/routes.enum';
+import { AppRoutes, UserRoutes } from '@Enums/routes.enum';
 
-import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
+    path: AppRoutes.Users,
+    children: [
+      {
+        path: UserRoutes.List,
+        loadComponent: () =>
+          import('./modules/users/components/list-users/list-users.component').then((c) => c.ListUsersComponent),
+      },
+      {
+        path: UserRoutes.Favorites,
+        loadComponent: () =>
+          import('./modules/users/components/favorite-users/favorite-users.component').then(
+            (c) => c.FavoriteUsersComponent,
+          ),
+      },
+      {
+        path: AppRoutes.Empty,
+        pathMatch: 'full',
+        redirectTo: UserRoutes.List,
+      },
+    ],
+  },
+  {
     path: AppRoutes.Empty,
-    component: AppComponent,
+    pathMatch: 'full',
+    redirectTo: `${AppRoutes.Users}/${UserRoutes.List}`,
   },
   {
     path: AppRoutes.Other,
--- src/app/shared/enums/routes.enum.ts
+++ src/app/shared/enums/routes.enum.ts
@@ -2,4 +2,10 @@ export enum AppRoutes {
   NotFound = '404',
   Other = '**',
   Empty = '',
+  Users = 'users',
+}
+
+export enum UserRoutes {
+  List = 'list',
+  Favorites = 'favorites',
 }
