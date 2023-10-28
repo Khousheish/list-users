485f96b93dfaf7dd275b8e80b4eb203cdb03fa00
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-10-28 20:54:33 +0200

    Implement Favorite User List Table

--- src/app/modules/users/components/favorite-users/favorite-users.component.ts
+++ src/app/modules/users/components/favorite-users/favorite-users.component.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { TableModule } from 'primeng/table';
+import { Observable } from 'rxjs';
+
+import { User } from '@Modules/users/shared/user.interface';
+import { UsersFacade } from '@Modules/users/store/users.facade';
 
 @Component({
   selector: 'lu-favorite-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule, AvatarModule, RouterModule],
   templateUrl: './favorite-users.component.html',
   styleUrls: ['./favorite-users.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FavoriteUsersComponent {}
+export class FavoriteUsersComponent {
+  protected favoriteUsers$: Observable<User[]> = this.userFacade.favoriteUsers$;
+
+  constructor(private readonly userFacade: UsersFacade) {}
+}
