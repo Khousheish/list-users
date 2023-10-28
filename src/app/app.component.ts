35dbcc839698442a2b305d625009645ee682c0f0
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-10-28 12:00:35 +0200

    Add Menu With Navigation Options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 import { environment } from '@Environment';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'ngt-root',
+  selector: 'lu-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
 })
 export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
   constructor(private readonly translateService: TranslateService) {}
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-users',
+        routerLink: '/users/list',
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        label: 'Favorites',
+        icon: 'pi pi-fw pi-star',
+        routerLink: '/users/favorites',
+        routerLinkActiveOptions: { exact: true },
+      },
+    ];
     this.translateService.setDefaultLang(environment.defaultLanguage);
 
     this.translateService.use(environment.defaultLanguage);
