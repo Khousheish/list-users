da2bed119cd5a92f061fb1789b87115766e370ff
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-10-31 21:48:08 +0100

    Move Api Url To Environment Configuration

--- src/app/modules/users/shared/users-repository.ts
+++ src/app/modules/users/shared/users-repository.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from '@Environment';
+
 import { PaginatedUsersResponse } from './user.interface';
 
 @Injectable({
@@ -11,6 +13,6 @@ export class UsersRepository {
   constructor(private readonly httpClient: HttpClient) {}
 
   getUsers(): Observable<PaginatedUsersResponse> {
-    return this.httpClient.get<PaginatedUsersResponse>(`https://reqres.in/api/users?per_page=10`);
+    return this.httpClient.get<PaginatedUsersResponse>(`${environment.apiUrl}/users?per_page=10`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ import { Environment } from '@Models/environment.models';
 export const environment: Environment = {
   production: false,
   defaultLanguage: 'en',
+  apiUrl: 'https://reqres.in/api',
 };
