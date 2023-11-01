b8e372463de7efba0734bed5189e9e53761eaeca
Hassan Khousheish <hassan.khousheish@crunchr.com>
2023-11-01 16:36:12 +0100

    Test Resolver

--- src/app/modules/users/components/list-users/list-users.resolver.spec.ts
+++ src/app/modules/users/components/list-users/list-users.resolver.spec.ts
@@ -1,17 +1,35 @@
-import { TestBed } from '@angular/core/testing';
-import { ResolveFn } from '@angular/router';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { BehaviorSubject, Observable, take } from 'rxjs';
+
+import { UsersFacade } from '@Modules/users/store/users.facade';
+import { Spied } from '@Specs/utils.types';
 
 import { listUsersResolver } from './list-users.resolver';
 
 describe('listUsersResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
-    TestBed.runInInjectionContext(() => listUsersResolver(...resolverParameters));
+  let mockedUsersFacade: Spied<UsersFacade>;
+  const mockedRoute = <ActivatedRouteSnapshot>{};
+  const pendingSubject = new BehaviorSubject<boolean>(false);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockedUsersFacade = {
+      ...jasmine.createSpyObj('UsersFacade', ['getUsers']),
+      usersPending$: pendingSubject.asObservable(),
+    };
+    TestBed.configureTestingModule({
+      providers: [{ provide: UsersFacade, useValue: mockedUsersFacade }],
+    });
   });
 
-  it('should be created', () => {
-    expect(executeResolver).toBeTruthy();
-  });
+  it('should call getUsers method of UsersFacade', fakeAsync(() => {
+    const resolver = <Observable<boolean>>TestBed.runInInjectionContext(() => {
+      return listUsersResolver(mockedRoute, <RouterStateSnapshot>{});
+    });
+
+    resolver.pipe(take(1)).subscribe(() => {});
+    tick();
+
+    expect(mockedUsersFacade.getUsers).toHaveBeenCalled();
+  }));
 });
